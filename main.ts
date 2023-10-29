f915a7d285b74319431a9a0e9cfd993cf336fcd3
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2023-10-29 17:47:34 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function InitHw () {
+    I2C_LCD1602.LcdInit(39)
+    pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+}
 function Start () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
     display(0, 0, "Hallo.")
@@ -6,6 +12,16 @@ function Start () {
     basic.pause(2000)
     I2C_LCD1602.clear()
 }
+pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
+    control.waitMicros(114)
+    basic.showIcon(IconNames.Meh)
+    serial.writeLine("" + (pins.i2cReadNumber(33, NumberFormat.UInt8LE, false)))
+})
+pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
+    control.waitMicros(114)
+    basic.showIcon(IconNames.Yes)
+    serial.writeLine("" + (pins.i2cReadNumber(34, NumberFormat.UInt8LE, false)))
+})
 function display (x: number, y: number, Text: string) {
     if (y == 2) {
         Y = 0
@@ -19,9 +35,14 @@ function display (x: number, y: number, Text: string) {
     }
     I2C_LCD1602.ShowString(Text, X, Y)
 }
+pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+    control.waitMicros(114)
+    basic.showIcon(IconNames.Heart)
+    serial.writeLine("" + (pins.i2cReadNumber(32, NumberFormat.UInt8LE, false)))
+})
 let X = 0
 let Y = 0
-I2C_LCD1602.LcdInit(39)
+InitHw()
 Start()
 basic.forever(function () {
 	

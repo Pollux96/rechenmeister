1811c906d9a2c8c31bb83a2a6601f75fe1a503b1
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2023-10-29 14:13:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,25 @@
-function macheEtwas (x: number, y: number, Text: string) {
+function Start (Text: string, x: number, Y: number) {
+    display(x, Y, Text)
+}
+function display (x: number, y: number, Text: string) {
     if (y == 2) {
         Y = 0
         X = x + 20
-    } else if (false) {
-    	
+    } else if (y == 3) {
+        Y = 1
+        X = x + 20
     } else {
-    	
+        Y = y
+        X = x
     }
     I2C_LCD1602.ShowString(Text, X, Y)
 }
 let X = 0
 let Y = 0
 I2C_LCD1602.LcdInit(39)
-macheEtwas(0, 0, "abc")
-macheEtwas(0, 1, "abc")
-macheEtwas(0, 2, "abc")
-macheEtwas(0, 3, "abc")
+Start("Hallo ich starte", 0, 0)
+Start("rechenkuenstler", 0, 1)
+basic.pause(1000)
 basic.forever(function () {
 	
 })

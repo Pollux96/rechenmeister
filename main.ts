4eae42967a33e664445d87659629b8459aa5b31b
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2023-11-06 23:15:39 +0100

    Tasten lesen funktioniert

--- main.ts
+++ main.ts
@@ -18,22 +18,27 @@ input.onButtonPressed(Button.A, function () {
     display(0, 1, "rechenolympiade")
     display(0, 2, "startet")
 })
-pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
-    control.waitMicros(1000)
-    taste = 255 - pins.i2cReadNumber(33, NumberFormat.UInt8LE, false)
-    if (taste > 0) {
-        serial.writeLine("P1")
-        serial.writeNumber(taste)
+function bestimmeZahlvonP2 (portWert: number) {
+    if (255 - portWert == 1) {
+        einerWert = 1
+    } else if (255 - portWert == 2) {
+        einerWert = 2
+    } else if (255 - portWert == 4) {
+        einerWert = 3
+    } else if (255 - portWert == 8) {
+        einerWert = 4
+    } else if (255 - portWert == 16) {
+        einerWert = 5
+    } else if (255 - portWert == 32) {
+        zehnerWert = 10
+    } else if (255 - portWert == 64) {
+        zehnerWert = 20
+    } else if (255 - portWert == 128) {
+        zehnerWert = 30
+    } else {
+        fehler = 1
     }
-})
-pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
-    control.waitMicros(1000)
-    taste = 255 - pins.i2cReadNumber(34, NumberFormat.UInt8LE, false)
-    if (taste > 0) {
-        serial.writeLine("P2")
-        serial.writeNumber(taste)
-    }
-})
+}
 function display (x: number, y: number, Text: string) {
     if (y == 2) {
         Y = 0
@@ -47,20 +52,66 @@ function display (x: number, y: number, Text: string) {
     }
     I2C_LCD1602.ShowString(Text, X, Y)
 }
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
-    control.waitMicros(1200)
-    serial.writeLine("Ereignis P0")
-    while (pins.digitalReadPin(DigitalPin.P0) == 0) {
-        taste = 255 - pins.i2cReadNumber(32, NumberFormat.UInt8LE, false)
-        serial.writeLine("Lese P0")
+function bestimmeZahlvonP1 (portWert: number) {
+    if (255 - portWert == 1) {
+        zehnerWert = 40
+    } else if (255 - portWert == 2) {
+        zehnerWert = 50
+    } else if (255 - portWert == 4) {
+        zehnerWert = 90
+    } else if (255 - portWert == 8) {
+        zehnerWert = 100
+    } else {
+        fehler = 1
     }
-    if (taste > 0) {
-        serial.writeLine("P0")
-        serial.writeNumber(taste)
+}
+function bestimmeZahlvonP0 (portWert: number) {
+    if (255 - portWert == 1) {
+        einerWert = 5
+    } else if (255 - portWert == 2) {
+        einerWert = 6
+    } else if (255 - portWert == 4) {
+        einerWert = 7
+    } else if (255 - portWert == 8) {
+        einerWert = 8
+    } else if (255 - portWert == 16) {
+        einerWert = 9
+    } else if (255 - portWert == 32) {
+        zehnerWert = 60
+    } else if (255 - portWert == 64) {
+        zehnerWert = 70
+    } else if (255 - portWert == 128) {
+        zehnerWert = 80
+    } else {
+        fehler = 1
     }
-})
+}
+let P2 = 0
+let P1 = 0
+let P0 = 0
 let X = 0
 let Y = 0
-let taste = 0
+let fehler = 0
+let zehnerWert = 0
+let einerWert = 0
 InitHw()
-Start()
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+        control.waitMicros(40000)
+        P0 = pins.i2cReadNumber(32, NumberFormat.UInt8LE, false)
+        bestimmeZahlvonP0(P0)
+    }
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        control.waitMicros(40000)
+        P1 = pins.i2cReadNumber(33, NumberFormat.UInt8LE, false)
+        bestimmeZahlvonP1(P1)
+    }
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
+        control.waitMicros(40000)
+        P2 = pins.i2cReadNumber(34, NumberFormat.UInt8LE, false)
+        bestimmeZahlvonP2(P2)
+    }
+})
